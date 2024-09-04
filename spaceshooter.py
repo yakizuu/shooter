03ec49965ce914f3eb13a5dcd9b2c6db21fc81f4
aleeya <m-10577081@moe-dl.edu.my>
2024-09-04 17:48:01 +0800

    added collision
    error in putting pics

--- spaceshooter.py
+++ spaceshooter.py
@@ -1,20 +1,20 @@
-#Space shooter 
-#Set up the screen
-
+  #Space shooter
 import turtle
 import os
+import math
+import random
 
 #Set up the screen
-ms = turtle.Screen()
-ms.bgcolor("black")
-ms.title("Space shooter")
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.title("Space Invaders")
 
 #Draw border
 border_pen = turtle.Turtle()
 border_pen.speed(0)
 border_pen.color("white")
 border_pen.penup()
-border_pen.setposition(-300, -300)
+border_pen.setposition(-300,-300)
 border_pen.pendown()
 border_pen.pensize(3)
 for side in range(4):
@@ -22,9 +22,22 @@ for side in range(4):
 	border_pen.lt(90)
 border_pen.hideturtle()	
 
+#Set the score to 0
+score = 0
+
+#Draw the score
+score_pen = turtle.Turtle()
+score_pen.speed(0)
+score_pen.color("white")
+score_pen.penup()
+score_pen.setposition(-290, 280)
+scorestring = "Score: %s" %score
+score_pen.write(scorestring, False, align="left", font=("Arial", 14, "normal"))
+score_pen.hideturtle()
+
 #Create the player turtle
 player = turtle.Turtle()
-player.color("yellow")
+player.color("blue")
 player.shape("triangle")
 player.penup()
 player.speed(0)
@@ -33,15 +46,27 @@ player.setheading(90)
 
 playerspeed = 15
 
-#Create the enemy
-enemy = turtle.Turtle()
-enemy.color("Brown")
-enemy.shape("circle")
-enemy.penup()
-enemy.speed(0)
-enemy.setposition(-200, 250)
+#Choose a number of enemies
+number_of_enemies = 5
+#Create an empty list of enemies
+enemies = []
+
+#Add enemies to the list
+for i in range(number_of_enemies):
+	#Create the enemy
+	enemies.append(turtle.Turtle())
+
+for enemy in enemies:
+	enemy.color("red")
+	enemy.shape("circle")
+	enemy.penup()
+	enemy.speed(0)
+	x = random.randint(-200, 200)
+	y = random.randint(100, 250)
+	enemy.setposition(x, y)
 
 enemyspeed = 2
+
 
 #Create the player's bullet
 bullet = turtle.Turtle()
@@ -61,7 +86,6 @@ bulletspeed = 20
 bulletstate = "ready"
 
 
-
 #Move the player left and right
 def move_left():
 	x = player.xcor()
@@ -76,7 +100,7 @@ def move_right():
 	if x > 280:
 		x = 280
 	player.setx(x)
-
+	
 def fire_bullet():
 	#Declare bulletstate as a global if it needs changed
 	global bulletstate
@@ -87,37 +111,71 @@ def fire_bullet():
 		y = player.ycor() + 10
 		bullet.setposition(x, y)
 		bullet.showturtle()
-		
 
+def isCollision(t1, t2):
+	distance = math.sqrt(math.pow(t1.xcor()-t2.xcor(),2)+math.pow(t1.ycor()-t2.ycor(),2))
+	if distance < 15:
+		return True
+	else:
+		return False
 #Create keyboard bindings
 turtle.listen()
 turtle.onkey(move_left, "Left")
 turtle.onkey(move_right, "Right")
 turtle.onkey(fire_bullet, "space")
 
-
 #Main game loop
 while True:
 	
-	#Move the enemy
-	x = enemy.xcor()
-	x += enemyspeed
-	enemy.setx(x)
-	
-	#Move the enemy back and down
-	if enemy.xcor() > 280:
-		y = enemy.ycor()
-		y -= 40
-		enemyspeed *= -1
-		enemy.sety(y)
+	for enemy in enemies:
+		#Move the enemy
+		x = enemy.xcor()
+		x += enemyspeed
+		enemy.setx(x)
 
-	if enemy.xcor() < -280:
-		y = enemy.ycor()
-		y -= 40
-		enemyspeed *= -1
-		enemy.sety(y)
+		#Move the enemy back and down
+		if enemy.xcor() > 280:
+			#Move all enemies down
+			for e in enemies:
+				y = e.ycor()
+				y -= 40
+				e.sety(y)
+			#Change enemy direction
+			enemyspeed *= -1
+		
+		if enemy.xcor() < -280:
+			#Move all enemies down
+			for e in enemies:
+				y = e.ycor()
+				y -= 40
+				e.sety(y)
+			#Change enemy direction
+			enemyspeed *= -1
+			
+		#Check for a collision between the bullet and the enemy
+		if isCollision(bullet, enemy):
+			#Reset the bullet
+			bullet.hideturtle()
+			bulletstate = "ready"
+			bullet.setposition(0, -400)
+			#Reset the enemy
+			x = random.randint(-200, 200)
+			y = random.randint(100, 250)
+			enemy.setposition(x, y)
+			#Update the score
+			score += 10
+			scorestring = "Score: %s" %score
+			score_pen.clear()
+			score_pen.write(scorestring, False, align="left", font=("Arial", 14, "normal"))
+		
+		if isCollision(player, enemy):
+			player.hideturtle()
+			enemy.hideturtle()
+			print ("Game Over")
+			break
 
-#Move the bullet
+		
+	#Move the bullet
 	if bulletstate == "fire":
 		y = bullet.ycor()
 		y += bulletspeed
@@ -127,9 +185,6 @@ while True:
 	if bullet.ycor() > 275:
 		bullet.hideturtle()
 		bulletstate = "ready"
-   
 
 
-
-
-delay = input("Press enter to finish")
+delay = raw_input("Press enter to finsh.")
